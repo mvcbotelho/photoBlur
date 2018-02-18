5e57ba6e9de40b0c86ef085f9aaaf9311ecbfc49
mvcbotelho <marcus.itec@gmail.com>
2018-02-17 22:18:11 -0300

    New corrections in script

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const inputs = document.querySelectorAll('.controls input')
 
 function handleUpdate(){
   const suffix = this.dataset.sizing || ''
-  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffixgit )
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate))
